7087c9dd74df0229c3d556bc5ae1efc67cf418aa
Kitsune <44873289+KitsuNeko256@users.noreply.github.com>
2020-03-20 00:19:23 +0300

    Fixed style errors

--- src/main/java/ru/itmo/java/HashTable.java
+++ src/main/java/ru/itmo/java/HashTable.java
@@ -9,22 +9,20 @@ public class HashTable {
     private static final double INITIAL_LOAD_FACTOR = 0.5;
     private static final int INITIAL_STEP = 137;
 
-    private static class Entry {
-        private Object key;
-        private Object value;
-
-        public Entry(Object key, Object value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
     private int capacity;
     private double loadFactor;
     private int size;
     private int notNullSize;
     private int step;
     private Entry[] elements;
+
+    public HashTable() {
+        this(INITIAL_CAPACITY, INITIAL_LOAD_FACTOR);
+    }
+
+    public HashTable(int inputCapacity) {
+        this(inputCapacity, INITIAL_LOAD_FACTOR);
+    }
 
     public HashTable(int inputCapacity, double inputLoadFactor) {
         this.capacity = Math.max(1, inputCapacity);
@@ -33,36 +31,12 @@ public class HashTable {
         this.elements = new Entry[this.capacity];
     }
 
-    public HashTable(int inputCapacity) {
-        this(inputCapacity, INITIAL_LOAD_FACTOR);
-    }
-
-    public HashTable() {
-        this(INITIAL_CAPACITY, INITIAL_LOAD_FACTOR);
-    }
-
-    private boolean isIndexEmpty(int index) {
-        return this.elements[index] == null || this.elements[index].key == null;
-    }
-
-    private int findIndex(Object key) {
-        int index = Math.abs(key.hashCode()) % this.capacity;
-        int firstEmpty = -1;
-        while (this.elements[index] != null && (this.elements[index].key == null || !this.elements[index].key.equals(key))) {
-            if (firstEmpty == -1 && this.isIndexEmpty(index)) {
-                firstEmpty = index;
-            }
-            index = (index + this.step) % this.capacity;
-        }
-        return index;
-    }
-
     Object get(Object key) {
         if (key == null) {
             return null;
         }
         int index = this.findIndex(key);
-        if (this.isIndexEmpty(index) || !this.elements[index].key.equals(key)) {
+        if (this.isIndexEmpty(index)) {
             return null;
         }
         return this.elements[index].value;
@@ -75,8 +49,9 @@ public class HashTable {
         int index = this.findIndex(key);
         // Element not found - add it
         if (this.isIndexEmpty(index)) {
-            if (this.elements[index] == null)
+            if (this.elements[index] == null) {
                 ++this.notNullSize;
+            }
             this.elements[index] = new Entry(key, value);
             ++this.size;
             this.ensureCapacity();
@@ -93,52 +68,81 @@ public class HashTable {
             return null;
         }
         int index = this.findIndex(key);
-        // Element not found - return null
         if (isIndexEmpty(index)) {
             return null;
         }
-        // Element is found - delete it
         Object previous = this.elements[index].value;
+        //mark current element as deleted
+        this.elements[index] = new Entry(null, null);
+        --size;
+        //if next element is null - replace this and all previous elements that are marked as deleted with nulls
+        //(optimization)
         if (this.elements[(index + this.step) % this.capacity] == null) {
-            this.elements[index].key = null;
-            while (this.elements[index] != null && this.elements[index].key == null) {
+            while (this.elements[index] != null && this.elements[index].isDeleted()) {
                 this.elements[index] = null;
                 index -= this.step;
                 if (index < 0) {
                     index += this.capacity;
                 }
             }
-        } else {
-            this.elements[index] = new Entry(null, null);
         }
-        --size;
         return previous;
     }
 
+    int size() {
+        return this.size;
+    }
+
+    private boolean isIndexEmpty(int index) {
+        return this.elements[index] == null || this.elements[index].isDeleted();
+    }
+
+    private int findIndex(Object key) {
+        int index = Math.abs(key.hashCode()) % this.capacity;
+        //if element is null or required element is found, end search
+        while (this.elements[index] != null
+                && !(!this.elements[index].isDeleted() && this.elements[index].key.equals(key))) {
+            index = (index + this.step) % this.capacity;
+        }
+        return index;
+    }
+
     private void ensureCapacity() {
-        if (this.size < this.capacity * this.loadFactor && this.notNullSize < this.capacity * Math.max(loadFactor, 0.70 ) && this.notNullSize < capacity) {
+        //recreate array if allowed size is exceeded or no nulls are left in the array
+        if (this.size < this.capacity * this.loadFactor && this.notNullSize < this.capacity) {
             return;
         }
 
         Entry[] tElements = this.elements;
 
-        this.size = 0;
-        this.notNullSize = 0;
-        if (this.size < this.capacity * this.loadFactor) {
+        //increase array size if array recreation is caused by exceeding size
+        if (this.size >= this.capacity * this.loadFactor) {
             this.capacity *= 2;
         }
+        this.size = 0;
+        this.notNullSize = 0;
         this.step = (this.capacity <= INITIAL_STEP) ? 1 : INITIAL_STEP;
         this.elements = new Entry[this.capacity];
 
         for (Entry tElement : tElements) {
-            if (tElement != null && tElement.key != null) {
+            if (tElement != null && !tElement.isDeleted()) {
                 this.put(tElement.key, tElement.value);
             }
         }
     }
 
-    int size() {
-        return this.size;
+    private static class Entry {
+        private final Object key;
+        private Object value;
+
+        public Entry(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        boolean isDeleted() {
+            return key == null;
+        }
     }
 
 }
